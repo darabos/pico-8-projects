9b7621c4851fc5f94557acc97ceff0a2c854a2ca
Daniel Darabos <darabos.daniel@gmail.com>
2023-02-24 23:21:03 +0100

    Talk to GPT-3.

---
+++ jipity-backend.js
@@ -0,0 +1,70 @@
+const functions = require('@google-cloud/functions-framework');
+const openaiLib = require("openai");
+
+const characters = [
+  {name: 'Jip'},
+  {
+    name: 'Rodrick', prefix: `
+Rodrick is an armored guard. He is standing guard in front of the royal bedroom.
+The queen has told Rodrick not to allow the king to be woken up for any reason.
+To be fair, this was several hours ago. It's almost noon, and Rodrick is starting
+to wonder how the king is able to sleep for so long.
+
+Jip is the son of the royal chef. He's 9 years old and always up to mischief.
+
+The foyer to the royal bedroom, where Rodrick stands, is richly decorated with
+colorful tapestries. A few barrels have been recently stacked in the corner.
+Rodrick hopes they will not become a permanent installation.
+
+Note the lack of long sentences.
+
+Here is a log of events:
+- Jip arrives in the foyer.
+- Rodrick says "What are you looking for, Jip?"
+- Jip says "Have you seen a donkey, sir?"
+- Rodrick says "No donkeys here, boy."
+- Jip says "You're the donkey!"
+- Jip leaves the foyer.
+- Rodrick says "I will get you, boy!"
+- Two hours pass.
+- Jip arrives in the foyer.
+    `,
+  },
+  {name: 'Zelenda', prefix: ''},
+];
+
+functions.http('act', async (req, res) => {
+  res.set('Access-Control-Allow-Origin', "*");
+  res.set('Access-Control-Allow-Methods', 'POST');
+  res.set('Access-Control-Allow-Headers', 'Content-Type');
+  if (req.body.id) {
+    const action = await getAction(req.body);
+    console.log(action);
+    res.send({action});
+  } else {
+    res.sendStatus(200);
+  }
+});
+
+const openai = new openaiLib.OpenAIApi(new openaiLib.Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+}));
+
+async function getAction(c) {
+  console.log(c);
+  const ch = characters[c.id];
+  const prompt = characters[1].prefix.trim() + '\n' + c.log.map(a => {
+    const actor = characters[a[0]].name;
+    if (a[1] === 'says') {
+      return `- ${actor} says "${a[2]}"`;
+    }
+  }).join('\n') + '\n-';
+  console.log(prompt);
+  const completion = await openai.createCompletion({
+    model: "text-davinci-003", prompt, stop: '\n-', max_tokens: 20 });
+  const text = completion.data.choices[0].text.trim();
+  const [actor, action, ...tail] = text.split(/ /);
+  if (actor === ch.name && action === 'says') {
+    return [c.id, 'says', tail.join(' ').replace(/"/g, '')];
+  }
+}
--- jipity-brain.js
+++ jipity-brain.js
@@ -1,14 +1,64 @@
-console.log('hello');
-function say(tile, text) {
-	pico8_gpio[50] = 3;
+function say(c, text) {
+	text = text.toLowerCase();
+	pico8_gpio[50] = c + 1;
 	pico8_gpio[51] = text.length;
 	for (let i=0; i<text.length; ++i) {
 		pico8_gpio[52+i] = text.charCodeAt(i);
 	}
 }
+
+const characters = [
+	{ log: [] },
+	{ log: [] },
+	{ log: [] },
+];
+for (let i = 0; i < characters.length; ++i) {
+	characters[i].id = i;
+}
+const player = characters[0];
+
+function playerSays(text) {
+	for (const c of characters) {
+		if (c == player) continue;
+		const dist = Math.hypot(c.x-player.x, c.y-player.y);
+		if (dist < 100) {
+			c.log.push([0, 'says', text]);
+			c.thinking = true;
+		}
+	}
+}
+
+async function getAction(c) {
+	const res = await fetch('http://localhost:8080/', {
+		method: 'POST',
+		headers: {'Content-Type': 'application/json'},
+		body: JSON.stringify({id: c.id, log: c.log}),
+	});
+	const j = await res.json();
+	if (j.action) {
+		const a = j.action;
+		c.log.push(a);
+		if (a[1] === 'says') {
+			say(a[0], a[2])
+		}
+	}
+	request = undefined;
+}
+
+let request;
 function think() {
 	setTimeout(think, 1000);
-	console.log(pico8_gpio[2], pico8_gpio[3]);
-	say(2, 'howdy?');
+	for (const c of characters) {
+		c.x = pico8_gpio[c.id*2+2];
+		c.y = pico8_gpio[c.id*2+3];
+		if (c.thinking && !request) {
+			request = getAction(c);
+			c.thinking = false;
+		}
+	}
+	if (pico8_gpio[50] == 1) {
+		const text = String.fromCharCode.apply(null, pico8_gpio.slice(52, 52 + pico8_gpio[51]));
+		playerSays(text);
+	}
 }
 think();
